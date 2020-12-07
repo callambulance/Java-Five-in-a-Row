91d34acf444f6cb68d014f9b1b1662a82078ae50
valerilysytsya <lashark11@gmail.com>
2020-12-07 19:45:15 +0200

    Added printBoard, getMove and mark functions

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -3,9 +3,20 @@ package com.codecool.fiveinarow;
 public class FiveInARow {
 
     public static void main(String[] args) {
-        Game game = new Game(11, 11);
-        game.enableAi(1);
-        game.enableAi(2);
-        game.play(5);
+        Game game = new Game(5, 5);
+        int player1 = 1;
+        int player2 = 2;
+        game.printBoard();
+        while (true) {
+            int [] player1Move = game.getMove(player1);
+            game.mark(player1, player1Move[0], player1Move[1]);
+            game.printBoard();
+            int [] player2Move = game.getMove(player2);
+            game.mark(player2, player2Move[0], player2Move[1]);
+            game.printBoard();
+        }
+//        game.enableAi(1);
+//        game.enableAi(2);
+//        game.play(5);
     }
 }
--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -1,10 +1,15 @@
 package com.codecool.fiveinarow;
+import java.util.Scanner;
 
 public class Game implements GameInterface {
+
+    //  Creating new scanner object which is used to get user input
+    private Scanner scanner = new Scanner(System.in);
 
     private int[][] board;
 
     public Game(int nRows, int nCols) {
+        this.board = new int[nRows][nCols];
     }
 
     public int[][] getBoard() {
@@ -16,7 +21,54 @@ public class Game implements GameInterface {
     }
 
     public int[] getMove(int player) {
-        return null;
+
+        int row = 0;
+        int col = 0;
+
+        boolean isInputValid = false;
+
+        while (isInputValid == false){
+            System.out.print("Player" + player + " turn: ");
+
+//          saving user input as a String
+            String userInput = scanner.nextLine();
+
+            if (userInput.length() != 2) {
+                System.out.println("Invalid number of characters, try again.");
+                continue;
+            } else if (!Character.isLetter(userInput.charAt(0))){
+                System.out.println("Invalid row character, try again.");
+                continue;
+            } else if (!Character.isDigit(userInput.charAt(1))){
+                System.out.println("Invalid column character, try again.");
+                continue;
+            }
+
+//          converting userInput[0] to numeric value
+            char upperLetter = Character.toUpperCase(userInput.charAt(0));
+            row = ((int)upperLetter - (int)'A');
+
+//          converting userInput[1] to int and substracting 1
+//          in order to get correct col index
+            col = Character.getNumericValue(userInput.charAt(1)) - 1;
+
+            if (row > board.length - 1) {
+                System.out.println("Invalid row index, try again.");
+                continue;
+            } else if (col > board[0].length - 1) {
+                System.out.println("Invalid column index, try again.");
+                continue;
+            } else if (board[row][col] != 0){
+                System.out.println("Already taken, try again.");
+                continue;
+            } else {
+                isInputValid = true;
+            }
+        }
+
+        int[] result = new int[] {row, col};
+
+        return result;
     }
 
     public int[] getAiMove(int player) {
@@ -24,6 +76,7 @@ public class Game implements GameInterface {
     }
 
     public void mark(int player, int row, int col) {
+        board[row][col] = player;
     }
 
     public boolean hasWon(int player, int howMany) {
@@ -35,6 +88,33 @@ public class Game implements GameInterface {
     }
 
     public void printBoard() {
+
+        char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+//      1st part prints col digits
+        System.out.println();
+        System.out.print(" ");
+        for (int colCounter = 1; colCounter <= board[0].length; colCounter++){
+            System.out.print("  " + colCounter);
+        }
+        System.out.println();
+
+//      2nd part prints a letter
+        for (int i = 0; i < board.length; i++){
+            System.out.print(alphabet[i] + "  ");
+//          and '.' if element is 0, 'X' if element is 1, 'O' if element is '2'
+            for (int j = 0; j < board[0].length; j++){
+                if (board[i][j] == 0) {
+                    System.out.print('.' + "  ");
+                } else if (board[i][j] == 1){
+                    System.out.print('X' + "  ");
+                } else {
+                    System.out.print('O' + "  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public void printResult(int player) {
