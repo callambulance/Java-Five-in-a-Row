bf92a16d52367cb54c555a073ce7d05397ee8a35
valerilysytsya <lashark11@gmail.com>
2020-12-09 12:09:48 +0200

    .

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -3,9 +3,19 @@ package com.codecool.fiveinarow;
 public class FiveInARow {
 
     public static void main(String[] args) {
-        Game game = new Game(11, 11);
-        game.enableAi(1);
-        game.enableAi(2);
-        game.play(5);
+        Game game = new Game(5, 5);
+
+        int howMany = 5;
+        game.play(howMany);
+
+//        while (true) {
+//        }
+
+
+//        game.enableAi(1);
+//        game.enableAi(2);
+//        game.play(5);
     }
 }
+
+
