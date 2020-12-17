478c3020a1e2d93ba05be33847fcacd01355d645
valerilysytsya <lashark11@gmail.com>
2020-12-17 21:16:59 +0200

    AI work randomly

--- src/main/java/com/codecool/fiveinarow/Player.java
+++ src/main/java/com/codecool/fiveinarow/Player.java
@@ -4,8 +4,8 @@ public class Player {
 
     private String name;
     private String color;
-    private int number;
-    private char playerType = 'H';
+    private final int number;
+    private char playerType;
 
     public Player(String name, int number, char playerType) {
         this.name = name;
@@ -13,21 +13,22 @@ public class Player {
         this.playerType = playerType;
     }
 
-    public Player(String name,String color, int number) {
+    public Player(String name,String color, int number, char playerType) {
         this.name = name;
         this.color = color;
         this.number = number;
+        this.playerType = playerType;
     }
 
     public String getName() { return name; }
 
-    public String getColor() { return color; }
+    public void setName(String name) { this.name = name; }
 
     public char getPlayerType(){ return playerType; }
 
     public void setPlayerType(char playerType) { this.playerType = playerType; }
 
-    public void setName(String name) { this.name = name; }
+    public String getColor() { return color; }
 
     public void setColor(String color) { this.color = color; }
 
